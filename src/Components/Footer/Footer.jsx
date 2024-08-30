a925165e3f1edf8a4ae419f11868ce79fffdf834
HaidarGhanem <haidar23ghanem@gmail.com>
2024-08-30 19:21:47 +0200

    edited

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -2,11 +2,13 @@ import './Footer.css'
 
 const Footer = () => {
   return (
-    <footer>
-        <div className="footer-container">
-            <p>Copyright &copy; 2024 Villa Agency Co., Ltd. All rights reserved.</p>
+    <>
+        <div className="footer">
+            <div className="footer__container">
+                <p>Copyright &copy; 2024 Villa Agency Co., Ltd. All rights reserved.</p>
+            </div>
         </div>
-    </footer>
+    </>
   )
 }
 
--- src/Components/Infobar/Inforbar.jsx
+++ src/Components/Infobar/Inforbar.jsx
@@ -1,30 +1,23 @@
-import './InfoBar.css'
+import './Infobar.css'
 
-const InfoBar = () => {
+const Infobar = () => {
   return (
-    <nav className="infobar">
+    <div className="infobar">
         <div className="infobar__container">
-            <div className="infobar__info">
-                <div className="infobar__mail">
-                    <img src="/icons/mail.png" alt="mail" className='topicon'  style={{paddingRight: "5px", paddingBottom:"4px"}}/>
-                    <span >info@company.com</span>
-                </div>
-                <div className="infobar__map">
-                    <img src="/icons/map.png" alt="map" className='topicon'  style={{paddingRight: "5px", paddingBottom:"5px"}}/>
-                    <span >Sunny Isles Beach, FL 33160</span>
-                </div>
-            </div>
-            
-            <ul className="infobar__menu">                                                                  
-                <li className="items"><div className="circle"><a href="#" className='infobar__links'><img src="/icons/facebook.png" alt="facebook" /></a></div></li>
-                <li className="items"><div className="circle"><a href="#" className='infobar__links'><img src="/icons/twitter.png" alt="twitter" /></a></div></li>
-                <li className="items"><div className="circle"><a href="#" className='infobar__links'><img src="/icons/linked.png" alt="linkedIn" /></a></div></li>
-                <li className="items"><div className="circle"><a href="#" className='infobar__links'><img src="/icons/instagram.png" alt="instagram" /></a></div></li>
+            <ul className="info__box">
+                <li><i className="fa fa-envelope"></i>info@company.com</li>
+                <li><i className="fa fa-map"></i> Sunny Isles Beach, FL 33160</li>
             </ul>
-            
+
+            <ul className="info__social">
+                <li><a href="#"><i className='fab fa-facebook'></i></a></li>
+                <li><a href="#"><i className='fab fa-twitter'></i></a></li>
+                <li><a href="#"><i className='fab fa-linkedin'></i></a></li>
+                <li><a href="#"><i className='fab fa-instagram'></i></a></li>
+            </ul>
         </div>
-    </nav>
+    </div>
   )
 }
 
-export default InfoBar
+export default Infobar
